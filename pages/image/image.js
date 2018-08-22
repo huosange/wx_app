df5a2797c921a3ce75306e9409636e17f7ae07d2
1070163966@qq.com <a883002b>
2018-08-22 11:07:34 +0800

    第二次提交
    主要进行功能的完善和bug修复

--- pages/image/image.js
+++ pages/image/image.js
@@ -5,20 +5,86 @@ Page({
    * 页面的初始数据
    */
   data: {
-    url:""
+    url:"",
+    loadingText:"加载中...",
+    toastText:"数据异常"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that=this;
+    that=this;
     if (options==null||options.url==null){
       return;
     }
 
-    this.setData({
+    that.setData({
       url:options.url
+    })
+  },
+
+/**
+ * 图片长按事件
+ */
+  onLongClick:function(event){
+  var mUrl="";
+  if(event.currentTarget.dataset.url!=null){
+    mUrl=event.currentTarget.dataset.url;
+  }
+
+    wx.showModal({
+      title: '是否保存到相册？',
+      comfirmText:'保存',
+      cancelText:'取消',
+      success:function(res){
+        if(res.confirm){
+          //点击了确定
+          saveImage(mUrl);
+        }else{
+          //点击了取消
+        }
+      }
     })
   }
 })
+
+var that;
+
+/**
+ * 保存图片
+ */
+function saveImage(mUrl){
+  that.setData({
+    loading:"下载中..."
+  });
+
+  wx.downloadFile({
+    url:mUrl,
+    type:'image',
+    success:function(res){
+      //下载成功后，保存到本地
+      wx.saveImageToPhotosAlbum({
+        filePath: res.tempFilePath,
+        success:function(res){
+          wx.showToast({
+            title: '保存成功',
+          })
+        },
+        fail:function(res){
+          wx.showToast({
+            title: '保存失败',
+          })
+        }
+      })
+    },
+    fail:function(res){
+      wx.showToast({
+        title: '保存失败，请重试',
+      })
+    },
+    complete:function(res){
+      console.log('download complete');
+    }
+  })
+}
